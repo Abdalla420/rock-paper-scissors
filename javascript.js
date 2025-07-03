59500b1d7b0c9cd5d2386a16edf210c1be2e808d
Abdalla420 <215827221+Abdalla420@users.noreply.github.com>
2025-07-03 07:18:12 -0400

    js

---
+++ javascript.js
@@ -0,0 +1,34 @@
+let humanScore = 0;
+let computerScore = 0;
+function getComputerChoice(){
+    const choices = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * 3);
+    return choices[random];
+}   
+function getHumanChoice(){
+    let choice = prompt("rock, paper, scissors");
+    return choice.toLowerCase();
+}
+
+function playRound(cChoice, hChoice){
+        cChoice = getComputerChoice();
+        hChoice = getHumanChoice();
+    if (cChoice == hChoice){
+        console.log("Draw");
+    }
+    else if(((cChoice == "rock") && (hChoice == "scissors")) || ((cChoice == "paper") && (hChoice == "rock")) || ((cChoice == "scissors") && (hChoice == "paper"))){
+        console.log("You lose! " + cChoice + " beats " + hChoice);
+        computerScore++;
+    }
+    else{
+        console.log("You win! " + hChoice + " beats " + cChoice);
+        humanScore++;
+    }
+}
+function playGame(){
+    for (let i = 0; i < 5; i++){
+        playRound();
+    }
+    console.log("Final score is " + humanScore + " to human, and " + computerScore + " to computer");
+}
+playGame();
